3e1f90f2ea45529d55d61c96f8f98d1501b12b0c
ayano_kondo@mail.toyota.co.jp <aya-no>
2023-06-21 20:28:55 +0900

    first

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "boltayano": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  }
+}
